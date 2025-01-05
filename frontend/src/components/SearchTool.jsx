fb560bbea73774bacc08c2a91b32903acf57c367
Dienhandsome <nguyenhoangdienst@gmail.com>
2025-01-05 20:24:41 +0700

    update mess

--- frontend/src/components/SearchTool.jsx
+++ frontend/src/components/SearchTool.jsx
@@ -19,13 +19,13 @@ function SearchTool() {
     e.preventDefault();
     if(!search) return;
     if(search.length < 3){
-      return toast.error('Tìm kiếm ít nhất ba từ')
+      return toast.error('Tìm kiếm ít nhất 3 từ')
     }
     const room = rooms.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()))
     if(room){
       setSelectedRoom(room)
       setSearch('');
-    } else toast.error("Không tìm thấy người dùng!")
+    } else toast.error("Không tìm thấy người dùngg!")
   }
   return (
     <Paper className='search-tool'
